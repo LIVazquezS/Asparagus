3a1b38feb669fe1a8fd5f2a6f899f29430eb32a2
Luis Itza Vazquez-Salazar <litzavazquezs@gmail.com>
2023-10-24 15:09:47 +0200

    Adding sampler for SN2 reaction

--- run_sn2.py
+++ run_sn2.py
@@ -6,6 +6,7 @@ from asparagus import Asparagus
 
 if True:
 # Sampling the TS, C-O and C-Br bond lengths are sampled.
+
     sampler = MetaSampler(
         config='sn2_config.json',
         sample_directory='sn2_samples',
@@ -14,23 +15,67 @@ if True:
         sample_calculator='XTB',
         sample_properties=['energy', 'forces', 'dipole'],
         sample_systems_optimize=False,
-        meta_cv=[[0,4],[0,5]],
-        meta_hookean=[[0, 4, 8.0],[0,5,8.0]],
-        meta_gaussian_height=0.05,
+        meta_cv=[[0,1],[0,5]],
+        meta_hookean=[[0, 1, 6.0],[0,5,6.0]],
+        meta_gaussian_height=0.10,
         meta_gaussian_widths=0.2,
         meta_gaussian_interval=10,
         meta_time_step=1.0,
-        meta_simulation_time=2000.0,
+        meta_simulation_time=5000.0,
+        meta_save_interval=10,
+        meta_temperature=300,
+        meta_langevin_friction=1.0,
+        meta_initial_velocities=True,
+        meta_initial_temperature=300.,
+    )
+    sampler.run()
+
+# For angle between Br-C-O
+# sampler = MetaSampler(
+#     config='sn2_config.json',
+#     sample_directory='sn2_samples',
+#     sample_systems=['data_sn2/fs_min.xyz'],
+#     sample_systems_format='xyz',
+#     sample_calculator='XTB',
+#     sample_properties=['energy', 'forces', 'dipole'],
+#     sample_systems_optimize=False,
+#     meta_cv=[[5,0, 1]],
+#     meta_hookean=[[5,0,1, 6.0]],
+#     meta_gaussian_height=0.10,
+#     meta_gaussian_widths=0.2,
+#     meta_gaussian_interval=10,
+#     meta_time_step=1.0,
+#     meta_simulation_time=5000.0,
+#     meta_save_interval=10,
+#     meta_temperature=300,
+#     meta_langevin_friction=1.0,
+#     meta_initial_velocities=True,
+#     meta_initial_temperature=300.,
+# )
+# sampler.run()
+
+if False:
+    sampler = MetaSampler(
+        config='sn2_config.json',
+        sample_directory='sn2_samples',
+        sample_systems=['data_sn2/pre_min.xyz'],
+        sample_systems_format='xyz',
+        sample_calculator='XTB',
+        sample_properties=['energy', 'forces', 'dipole'],
+        sample_systems_optimize=False,
+        meta_cv=[['-',0,1,0,2], [1, 2]],
+        meta_hookean=[[1, 2, 6.0]],
+        meta_gaussian_height=0.10,
+        meta_gaussian_widths=0.2,
+        meta_gaussian_interval=10,
+        meta_time_step=1.0,
+        meta_simulation_time=5000.0,
         meta_save_interval=10,
         meta_temperature=300,
         meta_langevin_friction=1.0,
         meta_initial_velocities=True,
         meta_initial_temperature=100.,
     )
-    sampler.run()
-
-if False:
-
 # This part samples the initial structures for the sn2 reaction, only methanol and bromomethane are sampled
 #Methanol
     sampler_met = MDsampler(
