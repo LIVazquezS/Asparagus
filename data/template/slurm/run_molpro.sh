eae0f875d5f596db5ff0c6a93d0d89540832feae
KToepfer <kai.toepfer90@gmail.com>
2024-03-12 15:30:02 +0100

    Slurm Calculator with Molpro example - Done

---
+++ data/template/slurm/run_molpro.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=molpro_test
+#SBATCH --partition=vshort
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1000
+
+#---------
+# Modules
+#---------
+
+# PC-Beethoven
+module load molpro/molpro2024-gcc-9.2.0
+# PC-Bach
+#module load molpro/molpro-2024.1
+
+#------------
+# Run Molpro
+#------------
+
+molpro -o run_molpro.out run_molpro.inp
+
+#-------------
+# Read Result
+#-------------
+
+python run_molpro.py run_molpro.xml
+
