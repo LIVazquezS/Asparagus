c9898dc1bb7bbfe3f815008718d2b08fde591544
KToepfer <kai.toepfer90@gmail.com>
2024-03-20 20:04:36 +0100

    start transfering PaiNN

--- asparagus/settings/__init__.py
+++ asparagus/settings/__init__.py
@@ -19,14 +19,6 @@ from .properties import (
     _alt_property_labels, _default_units, _default_output_block_options
 )
 
-from .globl import (
-    _global_mode, set_global_mode,
-    _global_config_file, set_global_config_file,
-    _global_dtype, set_global_dtype,
-    _global_device, set_global_device,
-    _global_rate, set_global_rate
-)
-
 from .config import (
     Configuration, get_config
 )
